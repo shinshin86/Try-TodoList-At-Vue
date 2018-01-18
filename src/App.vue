a703581077976fddd0455dd36cf9159acdf2ccd1
shinshin86 <beagles1986@gmail.com>
2018-01-18 21:26:18 +0900

    Use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <todo-header></todo-header>
     <todo-count></todo-count>
     <add-todo></add-todo>
     <todo-list></todo-list>
@@ -8,14 +7,12 @@
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
 import AddTodo from './components/AddTodo.vue'
 import TodoList from './components/TodoList.vue'
 import TodoCount from './components/TodoCount.vue'
 
 export default {
   components: {
-    TodoHeader,
     AddTodo,
     TodoList,
     TodoCount
--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-import Vue from 'vue';
-import App from './App.vue';
-import store from './store/index';
-
-import VueMaterial from 'vue-material';
+import Vue from 'vue'
+import store from './store/index'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
-Vue.use(VueMaterial);
+import VueRouter from 'vue-router'
+
+import App from './App.vue'
+import TodoHeader from './components/TodoHeader.vue'
+
+Vue.use(VueMaterial)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      components: {
+        header: TodoHeader,
+        main: App
+      }
+    }
+  ]
+})
 
 new Vue({
+  router,
   store,
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-});
+}).$mount('#app');
