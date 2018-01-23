889846cb0664b71fd77dfbdcb237140cab4e1a81
shinshin86 <beagles1986@gmail.com>
2018-01-24 06:26:09 +0900

    Set test

---
+++ test/index.test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai'
+import { actions, mutations } from '../src/store/index.js'
+
+describe('actions', () => {
+  it('DONE_CHECK', () => {
+
+  })
+  it('CHANGE_TITLE', () => {
+
+  })
+  it('ADD_TODO', () => {
+
+  })
+  it('SAVE_TODO', () => {
+
+  })
+  it('LOAD_TODO', () => {
+
+  })
+  it('TODO_COUNT', () => {
+
+  })
+  it('LOAD_DONE_TODO',() => {
+
+  })
+})
+
+describe('mutations', () => {
+  it('DONE_CHECK', () => {
+
+  })
+  it('CHANGE_TITLE', () => {
+
+  })
+  it('ADD_TODO', () => {
+
+  })
+  it('SAVE_TODO', () => {
+
+  })
+  it('LOAD_TODO', () => {
+
+  })
+  it('TODO_COUNT', () => {
+
+  })
+  it('LOAD_DONE_TODO',() => {
+
+  })
+})
